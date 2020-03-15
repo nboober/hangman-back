ec15740c2d14c99702c24a2ecd395cd532c807d7
Nick Boober <nboober@terpmail.umd.edu>
2020-03-15 10:57:15 -0400

    updated the api call to gather 50 words instead of just one

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,7 +1,7 @@
 class WordsController < ApplicationController
 
     def getword
-        word = RestClient.get("https://random-word-api.herokuapp.com/word?number=1&swear=1")
+        word = RestClient.get("https://random-word-api.herokuapp.com/word?number=50&swear=1")
 
         render json: word
     end
